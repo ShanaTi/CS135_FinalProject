94ca73f8e34422043c4b680cf44f67eaa6fd0ea4
Unknown <aznnoodle101@hotmail.com>
2020-12-09 10:58:30 -0800

    Basic scene setup
    * setup the basic scene
    * started some scripts, but didn't code them much yet
    * made some basic material colors to distinguish scene elements|
    * added spot lights to each room for lighting, will replace/modify directional lighting later
    * added elevators and doors
      * will work on these more later

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public enum CrewmateOrImposter {
+        Crewmate, Imposter
+    }
+    public CrewmateOrImposter team;
+
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+
+    private void FixedUpdate() {
+        
+    }
+}
